2daf6617538d05f9f29c2b4615280142428645e1
simensma <simensma@gmail.com>
2013-09-02 17:51:23 +0200

    Basic robot

---
+++ src/ProRobot.java
@@ -0,0 +1,20 @@
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+
+
+public class ProRobot extends Robot{
+	
+	@Override
+	public void run() {
+		while(true){
+			
+		}
+	}
+	
+	@Override
+	public void onScannedRobot(ScannedRobotEvent event) {
+		// TODO Auto-generated method stub
+		super.onScannedRobot(event);
+	}
+	
+}
