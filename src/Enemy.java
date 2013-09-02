0b9506df77276640759b48b297e258ce8c8f422c
Arnfnn Gjørvad <arnfinnr@stud.ntnu.no>
2013-09-02 18:59:25 +0200

    afqs

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,14 +1,24 @@
+import java.awt.geom.Point2D;
 import java.util.ArrayList;
 
 
 public class Enemy {
 	String name;
-	double xPos;
-	double yPos;
+	double time;
+	double speed;
+	double distance;
+	double bearing;
+	
+	double heading;
+	public double y;
+	public double x;
 	public ArrayList<Double> guessPosition(long time){
-		ArrayList<Double> pos = new ArrayList<>();
-		pos.add(xPos);
-		pos.add(yPos);
+		double diff = this.time - time;
+		double newY = y + Math.cos(heading) * speed * diff;
+		double newX = x + Math.sin(heading) * speed * diff;
+		ArrayList<Double> pos = new ArrayList<Double>();
+		pos.add(newX);
+		pos.add(newY);
 		return pos;
 	}
 }
--- src/ProRobot.java
+++ src/ProRobot.java
@@ -15,25 +15,19 @@ public class ProRobot extends AdvancedRobot{
 	boolean forward=true;
 	double speed=0.0;
 	HashMap<String, Enemy> enemies = new HashMap();
-	Enemy target;
+	Enemy target; 
 	@Override
 	public void run() {
 		
-        setAdjustGunForRobotTurn(true);
-        setAdjustRadarForGunTurn(true);
         setColors(new Color(90, 40, 12), Color.green, Color.green);   
 		
 		while(true){
 			doScan();
 			move();
-			shoot();
 			execute();
 		}
 	}
-	
-	private void shoot() {
-		
-	}
+
 
 	private void move() {
 		
@@ -52,27 +46,22 @@ public class ProRobot extends AdvancedRobot{
 		else
 			back(speed);
 			
-		
-		
-		double firePower = getFirePower();
-		long time = getTime() + (int)Math.round((Math.sqrt( Math.pow(getX()+target.xPos, 2) + Math.pow(getX()+target.xPos, 2))/(20-(3*firePower))));
-		ArrayList<Double> hitPos = target.guessPosition(time);
-		double gunOffset = getGunHeadingRadians() - (Math.PI/2 - Math.atan2(hitPos.get(0) - getY(), hitPos.get(1) - getX()));
-		setTurnGunLeftRadians(gunOffset);
+
 		}
 
-	private double getFirePower() {
-		return 3;
-	}
 
 
 	private void doScan() {
-		
+		setTurnRadarLeftRadians(2*Math.PI);
 	}
 
 	@Override
-	public void onScannedRobot(ScannedRobotEvent event) {
-		super.onScannedRobot(event);
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// TODO Auto-generated method stub
+		super.onScannedRobot(e);
+		if(e.getDistance()<300){
+			fire(2);
+		}
 	}
 	
 	
